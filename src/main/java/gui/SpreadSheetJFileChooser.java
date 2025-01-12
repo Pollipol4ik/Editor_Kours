31be98ed79343557b5d2c76ed72ba5d9e40b615c
Polina <polinakuptzova23@gmail.com>
2025-01-12 17:18:59 +0300

    корректировка форматов

--- src/main/java/gui/SpreadSheetJFileChooser.java
+++ src/main/java/gui/SpreadSheetJFileChooser.java
@@ -7,7 +7,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class SpreadSheetJFileChooser extends JFileChooser {
 
     private static final FileFilter[] fileFilters = {
-            new FileNameExtensionFilter("OpenDocument spreadsheet (*.spreadsheet)", "spreadsheet")
+            new FileNameExtensionFilter("OpenDocument spreadsheet (*.ods)", "ods"),
+            new FileNameExtensionFilter("Custom spreadsheet (*.spreadsheet)", "spreadsheet")
     };
 
     private static SpreadSheetJFileChooser spreadSheetJFileChooser;
@@ -16,6 +17,8 @@ public class SpreadSheetJFileChooser extends JFileChooser {
         for (FileFilter ff : fileFilters) {
             addChoosableFileFilter(ff);
         }
+        setAcceptAllFileFilterUsed(false); // Отключаем возможность выбора "All Files"
+        setFileFilter(fileFilters[0]); // Устанавливаем первый формат как дефолтный
     }
 
     public static SpreadSheetJFileChooser getSpreadSheetJFileChooser() {
